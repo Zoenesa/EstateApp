45d702981935ce6842e32518d8d0420e8f253d31
Zoenesa <zoenesa@gmail.com>
2019-01-25 23:00:05 +0700

    add filtersys

---
+++ FilterSys/Common.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilterSys
+{
+    public class Common
+    {
+        System.Windows.Forms.Form Owner;
+        public Common(System.Windows.Forms.Form Parent)
+        {
+            this.Owner = Parent;
+        }
+
+        public void ShowFilter()
+        {
+            formFilter filter = new formFilter();
+            filter.ShowDialog(this.Owner as System.Windows.Forms.IWin32Window);
+        }
+    }
+}
---
+++ FilterSys/formFilter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Telerik.WinControls.UI;
+
+namespace FilterSys
+{
+    public partial class formFilter : RadForm
+    {
+
+        public DataTable Table { get { return _Table; } }
+
+        private DataTable _Table;
+
+        public string Query { get { return _Query; } }
+
+        private string _Query;
+
+        public string Condition { get { return _Condition; } }
+
+        private string _Condition;
+
+        public formFilter()
+        {
+            InitializeComponent();
+        }
+
+        public formFilter(DataTable dataTable)
+        {
+            this._Table = dataTable;
+        }
+    }
+}
